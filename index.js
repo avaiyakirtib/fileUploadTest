c28b05c059588ee36defb06992059169c3e139ec
kirti <avaiyakirtib@gmail.com>
2023-12-09 10:29:22 +0530

    change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 const upload = multer();
 server.post("/", upload.single("file"), async (req, res) => {
   console.log(req.file, "insert");
-  if (req.file.mimetype === "image/png" || req.file.mimetype === "image/jpeg") {
+  if (req.file?.mimetype === "image/png" || req.file?.mimetype === "image/jpeg") {
     const base64String = req.file.buffer.toString("base64");
     return res.status(200).json({
       success: true,
